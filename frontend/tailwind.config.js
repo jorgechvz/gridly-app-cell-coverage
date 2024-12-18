824763b8c266dc1c778ed3ccb7b776f36a54a93f
jorgechvz <cha22060@byui.edu>
2024-12-18 14:59:04 -0500

    fix: update Tailwind CSS configuration to use ES module syntax and improve plugin import

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindcssAnimate from "tailwindcss-animate";
+export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -101,5 +102,5 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
